578992f27cce96d061e08e92d12975098c557b76
Reinaldo Omar <reymar.mendoza@gmail.com>
2022-01-29 16:46:22 -0500

    interactuar con las cards de la pagina de inicio

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const articleRouter = require('./routes/articles')
+const Article = require('./models/article')
 
 const app = express()
 
-mongoose.connect(
-	'mongodb+srv://reinaldo:Yv4Rn5baFDid50Es@clusternotes.1nlhq.mongodb.net/ClusterNotes'
-	// ,{ useNewUrlParser: true, useUnifiedTopology: true }
-)
+mongoose.connect('mongodb+srv://reinaldo:Yv4Rn5baFDid50Es@clusternotes.1nlhq.mongodb.net/ClusterNotes')
 
 // ejs es otro jsx, lo defini como el renderizador
 app.set('view engine', 'ejs')
@@ -17,26 +15,12 @@ app.use('/public', express.static('public'))
 // permitir que lea el body
 app.use(express.urlencoded({ extended: false }))
 
-// despues de la ruta articles se anaden las opciones del router
-app.use('/articles', articleRouter)
-
-app.get('/', (req, res) => {
-	const articles = [
-		{
-			title: 'Test article 1',
-			createdAt: new Date(),
-			description:
-				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ullam sint saepe hic debitis veniam, assumenda ea natus ipsum aperiam magnam delectus consequuntur esse sit earum vitae, distinctio eveniet aspernatur!',
-		},
-		{
-			title: 'Test article 2',
-			createdAt: new Date(),
-			description:
-				'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum dolor earum fugit molestiae, reprehenderit deleniti!',
-		},
-	]
+app.get('/', async (req, res) => {
+	const articles = await Article.find().sort({ createdAt: 'desc'})
 	res.render('articles/index', { articles: articles })
 })
 
+// despues de la ruta articles se anaden las opciones del router
+app.use('/articles', articleRouter)
 
 app.listen(5000)
