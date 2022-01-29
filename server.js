50e2fb9ca102963008696e072cd7e48791b7096e
Reinaldo Omar <reymar.mendoza@gmail.com>
2022-01-29 10:08:29 -0500

    inicio funcion de nueva nota

--- server.js
+++ server.js
@@ -11,16 +11,16 @@ app.get('/', (req, res) => {
 	const articles = [
 		{
 			title: 'Test article 1',
-			createdAt: Date.now(),
+			createdAt: new Date(),
 			description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ullam sint saepe hic debitis veniam, assumenda ea natus ipsum aperiam magnam delectus consequuntur esse sit earum vitae, distinctio eveniet aspernatur!',
 		},
 		{
 			title: 'Test article 2',
-			createdAt: Date.now(),
+			createdAt: new Date(),
 			description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum dolor earum fugit molestiae, reprehenderit deleniti!',
 		},
 	]
-	res.render('index', { articles: articles })
+	res.render('articles/index', { articles: articles })
 })
 
 // despues de la ruta articles se anaden las opciones del router
